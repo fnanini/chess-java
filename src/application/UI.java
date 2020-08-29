7d457866cc02f8fa1307d91b85b4fa3b2c326a4d
Felipe Nanini Cavelagna <felipenanini@gmail.com>
2020-08-29 03:01:09 -0300

    Correcting clear screen and implementing possible moves of Rook

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -31,9 +32,14 @@ public class UI {
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	
 	// https://stackoverflow.com/questions/2979383/java-clear-the-console
-	public static void clearScreen() {
-		System.out.print("\033[H\033[2J");
-		System.out.flush();
+	public static void clearScreen(){
+	    //Clears Screen in java
+	    try {
+	        if (System.getProperty("os.name").contains("Windows"))
+	            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+	        else
+	            Runtime.getRuntime().exec("clear");
+	    } catch (IOException | InterruptedException ex) {}
 	} 
 	
 	public static ChessPosition readChessPosition(Scanner sc) {
